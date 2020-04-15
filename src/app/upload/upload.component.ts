d989bcfd9b8e97f5b19b011db543b2f7a5e5f5e4
k40418132 <k40418132@gcloud.csu.edu.tw>
2020-04-16 00:05:24 +0800

    downloadUrl

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage'
 import { Observable } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
 
 import { RoomService } from '../Services/room.service'
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-upload',
@@ -11,9 +13,9 @@ import { RoomService } from '../Services/room.service'
   template: `
   <label for="file">File:</label>
   <input type="file" (change)="upload($event)" accept=".png,.jpg" />
-  <br>{{ downloadURL }}
+  <br><img src='{{ downloadURL|async }}' />
  `
- //<img src='{{ downloadURL }} />
+ 
 })
 export class UploadComponent implements OnInit {
 
@@ -25,13 +27,20 @@ export class UploadComponent implements OnInit {
   upload(event) {
     const randomId = Math.random().toString(36).substring(2);
     const file = event.target.files[0];
-    const filePath = 'name-your-file-path-here';
-    console.log(randomId);
-    const fileRef = this.storage.ref('images/'+randomId.toString()); // Add this line to get the path as a ref
-    const task = fileRef.put(file);
+    const filePath = 'images/'+randomId.toString();
+    console.log(filePath);
+    const fileRef = this.storage.ref(filePath); // Add this line to get the path as a ref
+    const task = this.storage.upload(filePath,file);
     this.uploadPercent = task.percentageChanges();
-    this.downloadURL = fileRef.getDownloadURL();
-    console.log(this.downloadURL);
+    task.snapshotChanges().pipe(
+      finalize(()=>{
+        this.downloadURL = fileRef.getDownloadURL();
+        this.downloadURL.subscribe(ursl=>{
+          this.RoomService.sendMsg(ursl,'test');
+        });
+        
+      })
+    ).subscribe();
   }
 
 
